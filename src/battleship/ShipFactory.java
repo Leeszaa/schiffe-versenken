80fe75bd9933fe1b87a9cdd086614b84f03c0422
Leesza <spheromany@arcor.de>
2024-08-12 17:08:05 +0200

    ShipFactory different Ship Classes added

--- src/battleship/ShipFactory.java
+++ src/battleship/ShipFactory.java
@@ -1,13 +1,30 @@
 package battleship;
 
 public interface ShipFactory {
-    Ship createShip(int size, boolean isHorizontal);
+    Ship createSchlachtschiff();
+    Ship createKreuzer();
+    Ship createZerstörer();
+    Ship createUBoot();
 }
 
 class DefaultShipFactory implements ShipFactory {
     @Override
-    public Ship createShip(int size, boolean isHorizontal) {
+    public Ship createSchlachtschiff() {
+        return new Ship(5, false); // Länge 5, zunächst vertikal
+    }
 
-        return new Ship(size, isHorizontal); 
+    @Override
+    public Ship createKreuzer() {
+        return new Ship(4, false); // Länge 4, zunächst vertikal
+    }
+
+    @Override
+    public Ship createZerstörer() {
+        return new Ship(3, false); // Länge 3, zunächst vertikal
+    }
+
+    @Override
+    public Ship createUBoot() {
+        return new Ship(2, false); // Länge 2, zunächst vertikal
     }
 }
