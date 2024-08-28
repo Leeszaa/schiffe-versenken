833be4b115dd9f18a138dd1cd2d69747d380c168
Leesza <spheromany@arcor.de>
2024-08-28 14:24:07 +0200

    Addes rules button, added ships left to place list

---
+++ src/battleship/factorys/player/ComputerPlayer.java
@@ -0,0 +1,92 @@
+/**
+ * @file LocalPlayer.java
+ *   Implementation of the IPlayer interface for a local player in the Battleship game.
+ */
+
+package battleship.factorys.player;
+
+import battleship.factorys.gameboard.IGameBoard;
+
+/**
+ * @class LocalPlayer
+ *   Represents a local player in the Battleship game.
+ * Implements the {@link IPlayer} interface.
+ */
+public class ComputerPlayer implements IPlayer {
+    private String name; /**< The name of the player */
+    private IGameBoard gameBoard; /**< The game board of the player */
+    private IGameBoard targetingBoard; /**< The targeting board of the player */
+
+    /**
+     *   Constructor for LocalPlayer.
+     * @param name The name of the player.
+     */
+    public ComputerPlayer(String name) {
+        this.name = name;
+    }
+
+    /**
+     *   Gets the name of the player.
+     * @return The name of the player.
+     */
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *   Places ships on the given game board.
+     * @param gameBoard The game board on which to place ships.
+     */
+    @Override
+    public void placeShips(IGameBoard gameBoard) {
+        // Implementation for placing ships
+    }
+
+    /**
+     *   Takes a turn by attacking the opponent's board.
+     * @param opponentBoard The opponent's game board.
+     * @return True if the turn was successful, false otherwise.
+     */
+    @Override
+    public boolean takeTurn(IGameBoard opponentBoard) {
+        // Implementation for taking a turn
+        return true;
+    }
+
+    /**
+     *   Sets the player's game board.
+     * @param gameBoard The game board to set.
+     */
+    @Override
+    public void setGameBoard(IGameBoard gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+
+    /**
+     *   Sets the player's targeting board.
+     * @param targetingBoard The targeting board to set.
+     */
+    @Override
+    public void setTargetingBoard(IGameBoard targetingBoard) {
+        this.targetingBoard = targetingBoard;
+    }
+
+    /**
+     *   Gets the player's game board.
+     * @return The player's game board.
+     */
+    @Override
+    public IGameBoard getGameBoard() {
+        return gameBoard;
+    }
+
+    /**
+     *   Gets the player's targeting board.
+     * @return The player's targeting board.
+     */
+    @Override
+    public IGameBoard getTargetingBoard() {
+        return targetingBoard;
+    }
+}
--- src/battleship/views/PlacementView.java
+++ src/battleship/views/PlacementView.java
@@ -29,6 +29,9 @@
      private JPanel parentPanel; /**< Parent panel containing this view */
      private ShipPlacementManager shipPlacementManager; /**< Manager for ship placement */
      private BattleshipGUI battleshipGUI; /**< Reference to the main GUI */
+     private JLabel[] shipCountLabels; /**< Labels for displaying ship counts */
+     private IPlayer player1; /**< The first player */
+     private IPlayer player2; /**< The second player */
  
      private static final int TOTAL_SHIPS = 10; /**< Total number of ships to be placed */
  
@@ -48,6 +51,8 @@
          this.parentPanel = parentPanel;
          this.battleshipGUI = battleshipGUI;
          this.shipPlacementManager = new ShipPlacementManager(player1, player2, this, battleshipGUI);
+            this.player1 = player1;
+            this.player2 = player2;
  
          initComponents();
      }
@@ -55,52 +60,111 @@
      /**
       *   Initializes the components of the view.
       */
-     private void initComponents() {
-         setLayout(new BorderLayout());
-         setBackground(Color.darkGray);
- 
-         JButton backButton = new JButton("Zurück");
-         backButton.addActionListener(e -> cardLayout.show(parentPanel, "MainMenuView"));
-         add(backButton, BorderLayout.NORTH);
- 
-         gridPanel = new JPanel(new GridLayout(10, 10));
-         gridPanel.setPreferredSize(new Dimension(600, 600));
-         gridCells = new JPanel[10][10];
- 
-         for (int i = 0; i < 10; i++) {
-             for (int j = 0; j < 10; j++) {
-                 JPanel cell = new JPanel();
-                 cell.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-                 cell.addMouseListener(new GridCellClickListener(i, j));
-                 gridCells[i][j] = cell;
-                 gridPanel.add(cell);
-             }
-         }
- 
-         JPanel placementWithLabels = new JPanel(new BorderLayout());
-         placementWithLabels.add(createColumnLabels(), BorderLayout.NORTH);
-         placementWithLabels.add(createRowLabels(), BorderLayout.WEST);
-         placementWithLabels.add(gridPanel, BorderLayout.CENTER);
-         placementWithLabels.setBackground(Color.darkGray);
- 
-         JLabel titleLabel = new JLabel("Schiffe platzieren. Klicke dafür auf eine Zelle...", SwingConstants.CENTER);
-         titleLabel.setFont(new Font("Roboto", Font.BOLD, 16));
-         titleLabel.setForeground(Color.WHITE);
- 
-         JPanel mainPanel = new JPanel(new GridBagLayout());
-         GridBagConstraints gbc = new GridBagConstraints();
-         gbc.gridx = 0;
-         gbc.gridy = 0;
-         gbc.anchor = GridBagConstraints.CENTER;
-         gbc.insets = new Insets(10, 10, 10, 10);
-         mainPanel.add(titleLabel, gbc);
-         mainPanel.setBackground(Color.darkGray);
- 
-         gbc.gridy = 1;
-         mainPanel.add(placementWithLabels, gbc);
- 
-         add(mainPanel, BorderLayout.CENTER);
-     }
+      private void initComponents() {
+        setLayout(new BorderLayout());
+        setBackground(Color.darkGray);
+    
+        JButton backButton = new JButton("Zurück");
+        backButton.addActionListener(e -> cardLayout.show(parentPanel, "MainMenuView"));
+        add(backButton, BorderLayout.NORTH);
+    
+        gridPanel = new JPanel(new GridLayout(10, 10));
+        gridPanel.setPreferredSize(new Dimension(600, 600));
+        gridCells = new JPanel[10][10];
+    
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                JPanel cell = new JPanel();
+                cell.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                cell.addMouseListener(new GridCellClickListener(i, j));
+                gridCells[i][j] = cell;
+                gridPanel.add(cell);
+            }
+        }
+    
+        JPanel placementWithLabels = new JPanel(new BorderLayout());
+        placementWithLabels.add(createColumnLabels(), BorderLayout.NORTH);
+        placementWithLabels.add(createRowLabels(), BorderLayout.WEST);
+        placementWithLabels.add(gridPanel, BorderLayout.CENTER);
+        placementWithLabels.setBackground(Color.darkGray);
+
+        JLabel instructionLabel = new JLabel(player1.getName() + ", platziere deine Schiffe.", SwingConstants.CENTER);
+        instructionLabel.setFont(new Font("Roboto", Font.BOLD, 16));
+        instructionLabel.setForeground(Color.WHITE);
+
+    
+        JLabel titleLabel = new JLabel("Schiffe platzieren. Klicke dafür auf eine Zelle...", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Roboto", Font.BOLD, 16));
+        titleLabel.setForeground(Color.WHITE);
+
+        JButton rulesButton = new JButton("Regeln");
+        rulesButton.addActionListener(e -> showRulesDialog());
+    
+        JPanel mainPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(10, 10, 10, 10);
+        mainPanel.add(instructionLabel, gbc);
+        mainPanel.setBackground(Color.darkGray);
+
+        gbc.gridy = 1;
+        mainPanel.add(titleLabel, gbc);
+    
+        gbc.gridy = 2;
+        mainPanel.add(placementWithLabels, gbc);
+    
+        // Add the ship list panel to the right of the grid panel
+        JPanel shipListPanel = createShipListPanel();
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        mainPanel.add(shipListPanel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        mainPanel.add(rulesButton, gbc);
+    
+        add(mainPanel, BorderLayout.CENTER);
+    }
+
+       private void showRulesDialog() {
+        JDialog dialog = new JDialog(battleshipGUI, "Regeln", true);
+        dialog.setLayout(new BorderLayout());
+        dialog.setSize(800, 400);
+        dialog.setLocationRelativeTo(battleshipGUI);
+        dialog.setResizable(false);
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        dialog.getContentPane().setBackground(Color.darkGray);
+    
+        // Create a panel to hold the labels
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+        contentPanel.setBackground(Color.darkGray);
+    
+        // Create and add labels with the desired font and color
+        String[] rules = {
+            "Regeln:",
+            "1. Jeder Spieler platziert abwechselnd seine Schiffe auf dem Spielfeld.",
+            "2. Die Schiffe dürfen nicht überlappen oder aneinander angrenzen.",
+            "3. Die Schiffe dürfen nicht diagonal platziert werden.",
+            "4. Der Spieler, der alle Schiffe des Gegners versenkt hat, gewinnt.",
+            "5. Klicke auf eine Zelle, um ein Schiff zu platzieren oder zu entfernen.",
+            "6. Jeder Spieler darf ein Schlachtschiff, zwei Kreuzer, drei Zerstörer und vier U-Boote platzieren.",
+            "7. Viel Spaß!"
+        };
+    
+        for (String rule : rules) {
+            JLabel label = new JLabel(rule);
+            label.setFont(new Font("Roboto", Font.BOLD, 16));
+            label.setForeground(Color.WHITE);
+            contentPanel.add(label);
+        }
+    
+        dialog.getContentPane().add(contentPanel, BorderLayout.CENTER);
+        dialog.setVisible(true);
+    }
  
      /**
       *   Creates the column labels for the grid.
@@ -135,6 +199,55 @@
          }
          return panel;
      }
+
+         private JPanel createShipListPanel() {
+        JPanel shipListPanel = new JPanel();
+        shipListPanel.setLayout(new BoxLayout(shipListPanel, BoxLayout.Y_AXIS));
+        shipListPanel.setBackground(Color.darkGray);
+    
+        String[] shipNames = { "Schlachtschiff", "Kreuzer", "Zerstörer", "U-Boot" };
+        int[] shipCounts = { 1, 2, 3, 4 }; // Example counts for each ship type
+        shipCountLabels = new JLabel[shipNames.length];
+    
+        JLabel titleLabel = new JLabel("Noch zu verteilende Schiffe:");
+        titleLabel.setFont(new Font("Roboto", Font.BOLD, 20));
+        titleLabel.setForeground(Color.WHITE);
+        shipListPanel.add(titleLabel);
+    
+        shipListPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+    
+        for (int i = 0; i < shipNames.length; i++) {
+            JPanel shipPanel = new JPanel(new BorderLayout());
+            shipPanel.setBackground(Color.darkGray);
+    
+            JLabel nameLabel = new JLabel(shipNames[i] + ": ");
+            nameLabel.setFont(new Font("Roboto", Font.BOLD, 16));
+            nameLabel.setForeground(Color.WHITE);
+    
+            shipCountLabels[i] = new JLabel("x" + shipCounts[i]);
+            shipCountLabels[i].setFont(new Font("Roboto", Font.BOLD, 16));
+            shipCountLabels[i].setForeground(Color.WHITE);
+    
+            shipPanel.add(nameLabel, BorderLayout.WEST);
+            shipPanel.add(shipCountLabels[i], BorderLayout.EAST);
+    
+            shipListPanel.add(shipPanel);
+            shipListPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+        }
+    
+        return shipListPanel;
+    }
+
+    private void updateShipCount(String shipName, int delta) {
+        String[] shipNames = { "Schlachtschiff", "Kreuzer", "Zerstörer", "U-Boot" };
+        for (int i = 0; i < shipNames.length; i++) {
+            if (shipNames[i].equals(shipName)) {
+                int currentCount = Integer.parseInt(shipCountLabels[i].getText().substring(1));
+                shipCountLabels[i].setText("x" + (currentCount + delta));
+                break;
+            }
+        }
+    }
  
      /**
       * @class GridCellClickListener
@@ -163,15 +276,16 @@
          public void mouseClicked(MouseEvent e) {
              IShip existingShip = shipPlacementManager.getShipAt(row, col);
              if (existingShip != null) {
-                 int confirm = JOptionPane.showConfirmDialog(PlacementView.this,
-                         "An dieser Stelle befindet sich bereits ein Schiff. Möchten Sie es löschen?",
-                         "Schiff löschen", JOptionPane.YES_NO_OPTION);
-                 if (confirm == JOptionPane.YES_OPTION) {
-                     shipPlacementManager.removeShip(row, col, existingShip, gridCells);
-                     gridCells[row][col].setBackground(null);
-                 }
-                 return;
-             }
+                int confirm = JOptionPane.showConfirmDialog(PlacementView.this,
+                        "An dieser Stelle befindet sich bereits ein Schiff. Möchten Sie es löschen?",
+                        "Schiff löschen", JOptionPane.YES_NO_OPTION);
+                if (confirm == JOptionPane.YES_OPTION) {
+                    shipPlacementManager.removeShip(row, col, existingShip, gridCells);
+                    gridCells[row][col].setBackground(null);
+                    updateShipCount(existingShip.getShipName(), 1); // Increment the counter
+                }
+                return;
+            }
  
              if (shipPlacementManager.getPlacedShipsCount() >= TOTAL_SHIPS) {
                  if (shipPlacementManager.getCurrentPlayer().equals("player1")) {
@@ -210,6 +324,7 @@
                  if (shipPlacementManager.canPlaceShip(selectedShip)) {
                      try {
                          shipPlacementManager.placeShip(row, col, shipSize, isHorizontal, selectedShip, gridCells);
+                         updateShipCount(selectedShip, -1);
                      } catch (IllegalArgumentException ex) {
                          JOptionPane.showMessageDialog(PlacementView.this, ex.getMessage());
                      }
